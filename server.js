d4c2a345b1cd34e684e7742e8ab0171a01bf6a9f
Kolton Decker <deckerman25@gmail.com>
2021-05-01 14:13:23 -0500

    Built basic folder structure and added empty files

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(

c881fad6dd6a7eca102e4c0f802ce11a4deef46c
Christina <christinalmoss@gmail.com>
2021-05-03 18:57:24 -0500

    added folders and files for React structure

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,29 @@
+function Footer() {
+    return (
+        <>
+            <footer class="page-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col l8 s12">
+                            <h5 class="white-text">Development Team</h5>
+                            <p class="grey-text text-lighten-4">We all got our start at KU's coding bootcamp!</p>
+                        </div>
+                        <div class="col l4 s12">
+                            <h5 class="white-text">Dev Team GitHub</h5>
+                            <ul>
+                                <li><a class="white-text" href="https://github.com/verusbabb">Steve Babb</a></li>
+                                <li><a class="white-text" href="https://github.com/koltondecker">Kolton Decker</a></li>
+                                <li><a class="white-text" href="https://github.com/cgouge93">Carly Gouge</a></li>
+                                <li><a class="white-text" href="https://github.com/cmoss703">Christina Moss</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;

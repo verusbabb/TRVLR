3a3fada550f756d3db5861d09df43a4ae19e959d
Kolton Decker <deckerman25@gmail.com>
2021-05-19 19:34:13 -0500

    fixing database

--- server.js
+++ server.js
@@ -33,6 +33,12 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/tripplanner",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  },
   function (err) {
     if (err) throw err;
 

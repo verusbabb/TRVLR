5e90986280b5b495fc23a419e1c9fa56a3f9acf2
Christina <christinalmoss@gmail.com>
2021-05-22 13:37:38 -0500

    formatting stuff

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -8,13 +8,13 @@ function index() {
     return (
         <div className="container valign-wrapper">
             <div className="row valign">
-                <div className="col l6 m5 s12">
+                <div className="col l12 m12 s12 center">
                     <div className="card-panel transparentBG no-shadows ">
                     <h1 className="black-text large-welcome">Welcome to TRVLR</h1>
                     <h1 className="mobile-welcome center-align">T R V L R</h1>
                     </div>
                 </div>
-                <div className="col l6 m5 s12">
+                <div className="col l12 s12 center">
                     <div className="card-panel transparentBG no-shadows mobile-action">
                     <h5 className="black-text">Plan your next trip with your friends - all in one place.</h5>
                     </div>
